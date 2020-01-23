97679bbdcb33aad43aca4cc41dd763952e466741
Ninja <matisalomone@hotmail.com>
2020-01-23 00:02:50 -0300

    Commit: Boton Ocultar/Mostrar
    Funciones en tipescript 
    Elemento NG if

--- src/app/directiva/directiva.component.ts
+++ src/app/directiva/directiva.component.ts
@@ -8,7 +8,24 @@ export class DirectivaComponent {
 
   listaCurso: String[] = ['Doom','Slayer','Rip','And','Tear'];
 
+  habilitar: Boolean = true ;
+  textoBoton: String = 'Ocultar' ;
 
   constructor() { }
+
+
+  setHabilitar(): void {
+    if (this.habilitar == true){
+      this.habilitar = false ; 
+      this.textoBoton = 'Mostrar' ;
+    } else {
+      this.habilitar = true ; 
+      this.textoBoton = 'Ocultar' ;
+
+    }
+
+    // this.habilitar = (this.habilitar == true) ? false : true 
+  }
+
 
 }
