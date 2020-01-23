20118df10a5f717c774fd1b4900ad7251694b3d3
Ninja <matisalomone@hotmail.com>
2020-01-22 23:43:16 -0300

    Commit: Componente directiva y NG for
    DoomSlayer Intensifies

--- src/app/directiva/directiva.component.ts
+++ src/app/directiva/directiva.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-directiva',
   templateUrl: './directiva.component.html',
-  styleUrls: ['./directiva.component.css']
-})
+ })
 export class DirectivaComponent {
+
+  listaCurso: String[] = ['Doom','Slayer','Rip','And','Tear'];
+
 
   constructor() { }
 
