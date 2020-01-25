430477ede9117b90d9d0cb8d71e0cfc2b527071f
Ninja <matisalomone@hotmail.com>
2020-01-25 00:55:58 -0300

    Commit: RouterModule y Routes
    Implementacion en el header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent} from './header/header.component';
-import { FooterComponent} from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClienteComponent } from './clientes/cliente.component';
 import { ClienteService } from './clientes/cliente.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: 'directiva', component: DirectivaComponent },
+  { path: 'clientes', component: ClienteComponent },
+];
+
+export const appRouting = RouterModule.forRoot(routes);
 
 
 @NgModule({
@@ -18,7 +27,8 @@ import { ClienteService } from './clientes/cliente.service';
     ClienteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ClienteService],
   bootstrap: [AppComponent]
